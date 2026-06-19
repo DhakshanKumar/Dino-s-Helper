b8a1787ecc5994cb6da4b3b5932b5f606c2dabd9
DhakshanKumar <dhakshankumarar@gmail.com>
2026-06-20 02:32:13 +0530

    owner lock

--- commands/spam.js
+++ commands/spam.js
@@ -3,7 +3,7 @@ import { SlashCommandBuilder, PermissionFlagsBits } from "discord.js";
 const command = {
   data: new SlashCommandBuilder()
     .setName("spam")
-    .setDescription("Spam mention a user (use responsibly)")
+    .setDescription("Spam mention a user (owner only)")
     .addUserOption(option =>
       option.setName("user")
         .setDescription("User to tag")
@@ -13,10 +13,19 @@ const command = {
       option.setName("count")
         .setDescription("Number of times to send")
         .setRequired(true)
-    )
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+    ),
 
   async execute(interaction) {
+    // 🔐 OWNER CHECK
+    const ownerIds = process.env.OWNER_IDS?.split(",") || [];
+
+    if (!ownerIds.includes(interaction.user.id)) {
+      return interaction.reply({
+        content: "❌ Only @Dinoplayz\_Official can use this command.",
+        ephemeral: true,
+      });
+    }
+
     const user = interaction.options.getUser("user");
     const count = interaction.options.getInteger("count");
 
