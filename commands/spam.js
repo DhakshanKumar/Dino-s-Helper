9ebddc98d1980a71a30426838a02f563c0348084
DhakshanKumar <dhakshankumarar@gmail.com>
2026-06-20 02:21:42 +0530

    spam

---
+++ commands/spam.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("spam")
+    .setDescription("Spam mention a user (use responsibly)")
+    .addUserOption(option =>
+      option
+        .setName("user")
+        .setDescription("User to tag")
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option
+        .setName("count")
+        .setDescription("Number of times to send")
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
+  async execute(interaction) {
+    const user = interaction.options.getUser("user");
+    const count = interaction.options.getInteger("count");
+
+    if (count > 10) {
+      return interaction.reply({
+        content: "Limit is 10 to avoid Discord rate limits.",
+        ephemeral: true,
+      });
+    }
+
+    await interaction.reply({
+      content: `Spamming ${user} ${count} times...`,
+      ephemeral: true,
+    });
+
+    for (let i = 0; i < count; i++) {
+      await interaction.channel.send(`${user}`);
+    }
+  },
+};
